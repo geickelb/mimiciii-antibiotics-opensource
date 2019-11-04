97b30f2d909b4669fe6f6c9f923c3274c56080ec
garrett <geickelb@github.com>
2019-11-04 12:05:24 -0600

    saving useful py files for pipeline
    parameters.py is used to import lots of useful parameters into each notebook. this allows for easier workflows

---
+++ packages_required.py
@@ -0,0 +1,18 @@
+import requests
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import psycopg2
+import collections
+import asyncio
+import getpass
+import re
+from datetime import datetime as dt
+import os,sys,re
+import urllib3
+import prettytable
+from collections import Counter
+import seaborn as sns
+import random
+
+
